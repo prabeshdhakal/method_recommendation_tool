bac3d84c40a92146555a41f78aa8fcdb10ec620d
prabeshdhakal <prfrbest@gmail.com>
2021-01-21 21:59:11 +0100

    added passenger_wsgi and Profcile for deployment

---
+++ passenger_wsgi.py
@@ -0,0 +1,13 @@
+from app import app
+
+application = app.server
+
+#import imp
+#import os
+#import sys
+
+
+#sys.path.insert(0, os.path.dirname(__file__))
+
+#wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
+#application = wsgi.application
